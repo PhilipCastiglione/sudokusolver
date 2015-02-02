a51feb1cd5b5cd8e960561d8f4c245d1b1df7f5d
Philip Castiglione <philipcastiglione@gmail.com>
2015-02-02 11:30:51 +1100

    Update sudokusolver_tests.py

--- tests/sudokusolver_tests.py
+++ tests/sudokusolver_tests.py
@@ -1,11 +1,4 @@
 from nose.tools import *
 import sudokusolver
 
-def setup():
-    print "setup"
-
-def teardown():
-    print "teardown"
-
-def test_basic():
-    print "run success"
+# not currently in use
